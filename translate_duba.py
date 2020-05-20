f84bb820401828767ea53a02d433ab61c7c7501a
liuzy <lzygzh@126.com>
2020-05-20 15:46:21 +0800

    change

--- translate_duba.py
+++ translate_duba.py
@@ -3,7 +3,7 @@
 #####################################
 # File name : translate_duba.py
 # Create date : 2019-08-24 10:59
-# Modified date : 2019-08-24 10:59
+# Modified date : 2020-05-20 15:45
 # Author : DARREN
 # Describe : not set
 # Email : lzygzh@126.com
@@ -13,6 +13,8 @@ from __future__ import print_function
 
 from urllib import request
 from lxml import etree
+from urllib.parse import quote
+import json
 
 class Translate:
     def __init__(self):
@@ -33,12 +35,26 @@ class Translate:
     '''解析翻译答案'''
     def extract_answers(self, content):
         selector = etree.HTML(content)
-        answer = selector.xpath('//div[@class="in-base"]/div/div/text()')[0]
+        json_text = selector.xpath('//script[@id="__NEXT_DATA__"]/text()')[0]
+        answer_dict = json.loads(json_text)
+        #print(answer_dict)
+        try:
+            answer = answer_dict["props"]["initialDvaState"]["word"]["wordInfo"]["baesInfo"]["translate_result"]
+            return answer
+        except Exception as e:
+            print("do not get answer")
+
+        try:
+            answer = answer_dict["props"]["initialDvaState"]["word"]["baesInfo"]["symbols"]["parts"]["means"][0]
+        except Exception as e:
+            print("do not get answer two")
+
         return answer
 
     '''翻译主函数'''
     def translate(self, query):
-        url = 'http://www.iciba.com/{}'.format(query)
+        url = 'http://www.iciba.com/{}'.format(quote(query, encoding='UTF-8'))
+        print(url)
         html = self.get_html(url)
         try:
             answer = self.extract_answers(html)
